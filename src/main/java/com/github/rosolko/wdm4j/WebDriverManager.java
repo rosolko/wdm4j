94f8e9a745209e94b4cf5fe55f7f2f3e703eb9cc
Aliaksandr Rasolka <rosolko@gmail.com>
2019-02-18 14:06:03 +0300

    Move initialization and configuration to builder

--- src/main/java/com/github/rosolko/wdm4j/WebDriverManager.java
+++ src/main/java/com/github/rosolko/wdm4j/WebDriverManager.java
@@ -15,64 +15,127 @@ import com.github.rosolko.wdm4j.service.FileService;
 import com.github.rosolko.wdm4j.service.PermissionService;
 import com.github.rosolko.wdm4j.service.UrlService;
 import com.github.rosolko.wdm4j.service.VariableService;
+import org.apache.logging.log4j.Logger;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.requireNonNull;
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 /**
  * @author Aliaksandr Rasolka
  * @since 1.0.0
  */
-public final class WebDriverManager {
-    private static WebDriverManager instance = new WebDriverManager();
+public class WebDriverManager {
+    private static final Logger log = getLogger();
 
-    private final UrlService urlService;
-    private final FileService fileService;
-    private final ArchiveService archiveService;
-    private final PermissionService permissionService;
-    private final VariableService variableService;
-
-    private final Queue<Path> queue = new ArrayBlockingQueue<>(10);
-
-    private WebDriverManager() {
-        this.urlService = new UrlService();
-        this.fileService = new FileService();
-        this.archiveService = new ArchiveService();
-        this.permissionService = new PermissionService();
-        this.variableService = new VariableService();
+    public static ConfigStep getInstance() {
+        return new Builder();
     }
 
-    public static synchronized WebDriverManager getInstance() {
-        return instance;
+    public interface ConfigStep {
+        SetupStep with(CommonConfig config);
     }
 
-    public void setup(final CommonConfig config) {
-        final Os os = Os.detect();
-        final Architecture architecture = Architecture.detect();
-        final String pattern = config.getUrlPattern();
-        final String version = config.getLatestVersion();
-        final String platform = config.getPlatform(os, architecture);
-        final Extension extension = config.getArchiveExtension(os);
-        final String binaryName = config.getBinaryName(os);
-        final String binaryVariable = config.getBinaryVariable();
-        final Path binaryPath = fileService.getBinaryPath(config.getBrowserName(), version, platform, binaryName);
+    public interface SetupStep {
+        SetupStep version(String version);
 
-        while (true) {
-            if (!queue.contains(binaryPath)) {
-                queue.add(binaryPath);
-                Path unarchivedBinaryPath;
-                if (!Files.exists(binaryPath)) {
-                    final URL url = urlService.buildUrl(pattern, version, platform, extension);
-                    final String archiveName = urlService.getFileNameFromUrl(url);
-                    final Path archiveTempPath = fileService.getArchiveTempPath(archiveName);
-                    final Path downloadedArchive = archiveService.download(url, archiveTempPath);
-                    unarchivedBinaryPath = archiveService.extract(downloadedArchive, binaryName, binaryPath, extension);
-                    archiveService.remove(downloadedArchive);
-                } else {
-                    unarchivedBinaryPath = binaryPath;
-                }
-                permissionService.makeExecutable(os, unarchivedBinaryPath);
-                variableService.setSystemProperty(binaryVariable, unarchivedBinaryPath);
-                break;
-            }
+        SetupStep os(Os os);
+
+        SetupStep architecture(Architecture architecture);
+
+        void setup();
+    }
+
+    private static class Builder implements ConfigStep, SetupStep {
+        private final UrlService urlService;
+        private final FileService fileService;
+        private final ArchiveService archiveService;
+        private final PermissionService permissionService;
+        private final VariableService variableService;
+        private final Queue<Path> queue = new ArrayBlockingQueue<>(Runtime.getRuntime().availableProcessors() / 2);
+
+        private CommonConfig config;
+        private String version;
+        private Os os;
+        private Architecture architecture;
+
+        private Builder() {
+            this.urlService = new UrlService();
+            this.fileService = new FileService();
+            this.archiveService = new ArchiveService();
+            this.permissionService = new PermissionService();
+            this.variableService = new VariableService();
         }
-        queue.remove(binaryPath);
+
+        @Override
+        public SetupStep with(final CommonConfig config) {
+            requireNonNull(config, "config must not be null");
+
+            log.info("Set '{}' config", config);
+
+            this.config = config;
+            return this;
+        }
+
+        @Override
+        public SetupStep version(final String version) {
+            this.version = version;
+            return this;
+        }
+
+        @Override
+        public SetupStep os(final Os os) {
+            this.os = os;
+            return this;
+        }
+
+        @Override
+        public SetupStep architecture(final Architecture architecture) {
+            this.architecture = architecture;
+            return this;
+        }
+
+        @Override
+        public void setup() {
+            if (isNull(this.version)) {
+                this.version = config.getLatestVersion();
+            }
+            if (isNull(this.os)) {
+                this.os = Os.detect();
+            }
+            if (isNull(this.architecture)) {
+                this.architecture = Architecture.detect();
+            }
+
+            log.info("Setup binary with '{}' version, '{}' os and '{}' architecture", version, os, architecture);
+
+            final String pattern = config.getUrlPattern();
+            final String platform = config.getPlatform(os, architecture);
+            final Extension extension = config.getArchiveExtension(os);
+            final String binaryName = config.getBinaryName(os);
+            final String binaryVariable = config.getBinaryVariable();
+            final Path binaryPath = fileService.getBinaryPath(config.getBrowserName(), version, platform, binaryName);
+
+            while (true) {
+                if (!queue.contains(binaryPath)) {
+                    queue.add(binaryPath);
+                    Path unarchivedBinaryPath;
+                    if (!Files.exists(binaryPath)) {
+                        final URL url = urlService.buildUrl(pattern, version, platform, extension);
+                        final String archiveName = urlService.getFileNameFromUrl(url);
+                        final Path archiveTempPath = fileService.getArchiveTempPath(archiveName);
+                        final Path downloadedArchive = archiveService.download(url, archiveTempPath);
+                        unarchivedBinaryPath = archiveService.extract(downloadedArchive, binaryName, binaryPath, extension);
+                        archiveService.remove(downloadedArchive);
+                    } else {
+                        unarchivedBinaryPath = binaryPath;
+                    }
+                    permissionService.makeExecutable(os, unarchivedBinaryPath);
+                    variableService.setSystemProperty(binaryVariable, unarchivedBinaryPath);
+                    break;
+                }
+            }
+            queue.remove(binaryPath);
+        }
     }
 }
--- src/test/java/com/github/rosolko/wdm4j/WebDriverManagerTests.java
+++ src/test/java/com/github/rosolko/wdm4j/WebDriverManagerTests.java
@@ -14,13 +14,12 @@ import com.github.rosolko.wdm4j.config.impl.PhantomJsConfig;
 import com.github.rosolko.wdm4j.enums.Architecture;
 import com.github.rosolko.wdm4j.enums.Os;
 import com.github.rosolko.wdm4j.service.FileService;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import static org.apache.logging.log4j.LogManager.getLogger;
 
 /**
  * @author Aliaksandr Rasolka
@@ -28,7 +27,10 @@ import static org.apache.logging.log4j.LogManager.getLogger;
  */
 @DisplayName("WebDriver manager tests")
 class WebDriverManagerTests {
-    private static final Logger log = getLogger();
+    private Os os;
+    private Architecture architecture;
+    private FileService fileService;
+    private FirefoxConfig firefoxConfig;
 
     private static Stream<CommonConfig> createConfigs() {
         return Stream.of(
@@ -41,22 +43,186 @@ class WebDriverManagerTests {
         );
     }
 
-    @ParameterizedTest(name = "{arguments}")
-    @MethodSource("createConfigs")
-    @DisplayName("Get and put binary to system variables")
-    void ableToSetUpBinary(final CommonConfig config) {
-        log.info("Try to setup WebDriver binary based on '{}' config", config.getClass().getSimpleName());
+    @DisplayName("Detect os and architecture, initialize file service and default config")
+    @BeforeEach
+    void setUp() {
+        os = Os.detect();
+        architecture = Architecture.detect();
+        fileService = new FileService();
+        firefoxConfig = new FirefoxConfig();
+    }
 
-        final FileService fileService = new FileService();
+    @ParameterizedTest
+    @MethodSource("createConfigs")
+    @DisplayName("Setup binary for specific config")
+    void ableToSetUpBinary(final CommonConfig config) {
         final String version = config.getLatestVersion();
-        final Os os = Os.detect();
-        final Architecture architecture = Architecture.detect();
         final String platform = config.getPlatform(os, architecture);
         final String binaryName = config.getBinaryName(os);
         final Path binaryPath = fileService.getBinaryPath(config.getBrowserName(), version, platform, binaryName);
         final String variable = config.getBinaryVariable();
 
-        WebDriverManager.getInstance().setup(config);
+        WebDriverManager.getInstance().with(config).setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override version and setup binary")
+    void ableToOverrideVersion() {
+        final String version = "v0.23.0";
+
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .version(version)
+            .setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override os and setup binary")
+    void ableToOverrideOs() {
+        final Os os = Os.linux;
+
+        final String version = firefoxConfig.getLatestVersion();
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .os(os)
+            .setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override architecture and setup binary")
+    void ableToOverrideArchitecture() {
+        final Architecture architecture = Architecture.x86_32;
+
+        final String version = firefoxConfig.getLatestVersion();
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .architecture(architecture)
+            .setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override os and architecture and setup binary")
+    void ableToOverrideOsAndArchitecture() {
+        final Os os = Os.linux;
+        final Architecture architecture = Architecture.x86_32;
+
+        final String version = firefoxConfig.getLatestVersion();
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .os(os)
+            .architecture(architecture)
+            .setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override version and architecture and setup binary")
+    void ableToOverrideVersionAndArchitecture() {
+        final String version = "v0.22.0";
+        final Architecture architecture = Architecture.x86_64;
+
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .version(version)
+            .architecture(architecture)
+            .setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override version and os and setup binary")
+    void ableToOverrideVersionAndOs() {
+        final String version = "v0.22.0";
+        final Os os = Os.windows;
+
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .version(version)
+            .os(os)
+            .setup();
+
+        final String systemVariable = System.getProperty(variable);
+        Assertions.assertTrue(Files.exists(binaryPath));
+        Assertions.assertNotNull(systemVariable);
+        Assertions.assertEquals(systemVariable, binaryPath.toString());
+    }
+
+    @Test
+    @DisplayName("Override version, os and architecture and setup binary")
+    void ableToOverrideVersionAndOsAndArchitecture() {
+        final String version = "v0.22.0";
+        final Os os = Os.linux;
+        final Architecture architecture = Architecture.x86_32;
+
+        final String platform = firefoxConfig.getPlatform(os, architecture);
+        final String binaryName = firefoxConfig.getBinaryName(os);
+        final Path binaryPath = fileService.getBinaryPath(firefoxConfig.getBrowserName(), version, platform, binaryName);
+        final String variable = firefoxConfig.getBinaryVariable();
+
+        WebDriverManager.getInstance()
+            .with(firefoxConfig)
+            .version(version)
+            .os(os)
+            .architecture(architecture)
+            .setup();
 
         final String systemVariable = System.getProperty(variable);
         Assertions.assertTrue(Files.exists(binaryPath));
